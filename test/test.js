8fe83d8a6f041092e895519ecd16d378becf4280
Vladimir D <vladimir@brandlight.org>
2023-05-16 19:05:53 +0400

    tests updated to work with OIDC auth

--- test/test.js
+++ test/test.js
@@ -59,34 +59,61 @@ describe('Application life cycle test', function () {
         await browser.get(`https://${app.fqdn}/accounts/login/`);
         await browser.sleep(2000);
 
-        await browser.findElements(By.xpath('//a[@class="dropdown-item" and contains(., "Log out")]')).then(function(elems) {
-            browser.get(`https://${app.fqdn}/accounts/logout/`);
-            browser.sleep(2000);
-            browser.get(`https://${app.fqdn}/accounts/login/`);
-            browser.sleep(2000);
-        });
+        if (await browser.findElements(By.xpath('//a[@class="dropdown-item" and contains(., "Log out")]')).then(found => !!found.length)) {
+            await browser.get(`https://${app.fqdn}/accounts/logout/`);
+            await browser.sleep(2000);
+            await browser.get(`https://${app.fqdn}/accounts/login/`);
+            await browser.sleep(2000);
+        }
 
-        await browser.wait(until.elementLocated(By.xpath('//input[@name="login"]')));
-        await browser.findElement(By.xpath('//input[@name="login"]')).sendKeys(username);
-        await browser.findElement(By.xpath('//input[@name="password"]')).sendKeys(password);
-        await browser.sleep(2000);
-        await browser.findElement(By.xpath('//button[@type="submit" and contains(., "Sign In")]')).click();
-        await browser.sleep(5000);
+        /* native appp auth
+        if (await browser.findElements(By.xpath('//input[@name="username"]')).then(found => !!found.length)) {
+            await browser.findElement(By.xpath('//input[@name="login"]')).sendKeys(username);
+            await browser.findElement(By.xpath('//input[@name="password"]')).sendKeys(password);
+            await browser.sleep(2000);
+            await browser.findElement(By.xpath('//button[@type="submit" and contains(., "Sign In")]')).click();
+            await browser.sleep(2000);
+        }
+        */
 
-        await browser.findElements(By.xpath('//input[@value="Create Space"]')).then(function(elems) {
-            browser.findElement(By.xpath('//input[@value="Create Space"]')).click();
+        // OIDC
+        if (await browser.findElements(By.xpath('//button[contains(text(), "Sign in using Cloudron")]')).then(found => !!found.length)) {
+            await browser.findElement(By.xpath('//button[contains(text(), "Sign in using Cloudron")]')).click();
+            await browser.sleep(2000);
+            await browser.findElement(By.xpath('//button[contains(text(), "Continue")]')).click();
+            await browser.sleep(2000);
+        }
+
+        if (await browser.findElements(By.xpath('//input[@name="username"]')).then(found => !!found.length)) {
+            await browser.findElement(By.xpath('//input[@name="username"]')).sendKeys(username);
+            await browser.findElement(By.xpath('//input[@name="password"]')).sendKeys(password);
+            await browser.findElement(By.xpath('//button[@type="submit" and contains(text(), "Log in")]')).click();
+            await browser.sleep(2000);
+        }
+
+        if (await browser.findElements(By.xpath('//button[@type="submit" and contains(text(), "Authorize")]')).then(found => !!found.length)) {
+            await browser.findElement(By.xpath('//button[@type="submit" and contains(text(), "Authorize")]')).click();
+            await browser.sleep(2000);
+        }
+
+        if (await browser.findElements(By.xpath('//input[@value="Create Space"]')).then(found => !!found.length)) {
+            await browser.findElement(By.xpath('//input[@value="Create Space"]')).click();
             browser.sleep(3000);
-        });
+        }
 
+        if (await browser.findElements(By.xpath('//p[@class="card-text" and contains(., "Create a new recipe directly in Tandoor.")]')).then(found => !!found.length) ||
+            await browser.findElements(By.xpath('//div[contains(@class, "alert-success") and contains(., "Successfully signed in as ' + username + '")]')).then(found => !!found.length)) {
+            return 1;
+        }
+        return 0;
 //        await browser.wait(until.elementLocated(By.xpath('//p[@class="card-text" and contains(., "Create a new recipe directly in Tandoor.")]')), TIMEOUT);
-        await browser.wait(until.elementLocated(By.xpath('//div[contains(@class, "alert-success") and contains(., "Successfully signed in as ' + username + '")]')), TIMEOUT);
- 
+//        await browser.wait(until.elementLocated(By.xpath('//div[contains(@class, "alert-success") and contains(., "Successfully signed in as ' + username + '")]')), TIMEOUT);
     }
 
     async function getMainPage() {
         await browser.get('https://' + app.fqdn + '/');
         await browser.sleep(5000);
-        await browser.wait(until.elementLocated(By.id('collapse_advanced_search')), TIMEOUT);
+        await browser.wait(until.elementLocated(By.xpath('//*[@id="collapse_advanced_search" or @aria-controls="collapse_advanced_search"]')), TIMEOUT);
     }
 
     async function createRecipe(recipename) {
